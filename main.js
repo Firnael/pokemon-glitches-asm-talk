9019423a583da0be495714220c680dd57bf29b2d
Audren Burlot <audren.burlot@maxds.fr>
2022-10-05 22:49:33 +0200

    fin du xp underflow et chen glitch

--- main.js
+++ main.js
@@ -4,10 +4,12 @@ import 'reveal.js/dist/reveal.css'
 //  beige, black, blood, league, moon, night, serif, simple, ...
 import 'reveal.js/dist/theme/blood.css'
 import 'reveal.js/plugin/highlight/monokai.css'
+import './style.css'
 
 import Reveal from 'reveal.js'
 import RevealNotes from 'reveal.js/plugin/notes/notes'
 import RevealHighlight from 'reveal.js/plugin/highlight/highlight'
+import RevealMath from 'reveal.js/plugin/math/math'
 
 const deck = new Reveal()
 deck.initialize({
@@ -15,6 +17,7 @@ deck.initialize({
     slideNumber: false,
     plugins: [
         RevealNotes,
-        RevealHighlight
+        RevealHighlight,
+        RevealMath.KaTeX
     ]
 })
