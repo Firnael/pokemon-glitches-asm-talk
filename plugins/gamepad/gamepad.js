ac3422ffe726177148574ca6a69fbb29774dc6f6
Audren <audren.burlot@gmail.com>
2023-10-25 01:17:41 +0200

    le joycon le plus chaud de ta région

--- plugins/gamepad/gamepad.js
+++ plugins/gamepad/gamepad.js
@@ -48,30 +48,45 @@
                                   : typeof obj;
                           };
 
-                function init (instance) {
+                function init(instance) {
                     var Reveal = instance;
                     var haveEvents = 'ongamepadconnected' in window;
                     var controllers = {};
                     var cooldownedButtons = [];
 
+                    const COOLDOWN = 300; // in ms
+
                     // Comments : Joycon bindings
                     var BUTTON = {
-                        A: 0,  // A
-                        B: 1,  // X
-                        X: 2,  // B
-                        Y: 3,  // Y
-                        LB: 4, // SL
-                        RB: 5, // SR
+                        A: 0,
+                        B: 1,
+                        X: 2,
+                        Y: 3,
+                        LB: 4,
+                        RB: 5,
                         LT: 6,
-                        RT: 7, // ZR
-                        BACK: 8, // R
-                        START: 9, // +
-                        LEFTSTICK: 10, // STICKCLICK
+                        RT: 7,
+                        BACK: 8,
+                        START: 9,
+                        LEFTSTICK: 10,
                         RIGHTSTICK: 11,
                         DUP: 12,
                         DDOWN: 13,
                         DLEFT: 14,
                         DRIGHT: 15
+                    };
+
+                    var RJ_BUTTON = {
+                        A: 0,
+                        X: 1,
+                        B: 2,
+                        Y: 3,
+                        SL: 4,
+                        SR: 5,
+                        ZR: 7,
+                        R: 8,
+                        PLUS: 9,
+                        STICK: 10
                     };
 
                     var AXIS = {
@@ -81,12 +96,19 @@
                         ROY: 3
                     };
 
+                    var RJ_AXIS = {
+                        LOX: 0,
+                        LOY: 1,
+                        ROX: 2,
+                        ROY: 3
+                    };
+
                     var pointing = false;
                     var pointer = document.createElement('div');
                     pointer.style.position = 'absolute';
-                    pointer.style.width = '1px';
-                    pointer.style.height = '1px';
-                    pointer.style.boxShadow = '0 0 7px 4px #f00';
+                    pointer.style.width = '20px';
+                    pointer.style.height = '20px';
+                    pointer.style.boxShadow = '3px 2px 2px #333';
                     pointer.style.background = '#f00';
                     pointer.style.top = 0;
                     pointer.style.left = 0;
@@ -103,7 +125,7 @@
                                 cooldownedButtons = cooldownedButtons.filter(function (v) {
                                     return v !== b;
                                 });
-                            }, 500);
+                            }, COOLDOWN);
 
                             return true;
                         }
@@ -155,68 +177,8 @@
                                     }
 
                                     if (pressed && cooldown('button-' + i)) {
-                                       console.log(i);
-                                        switch (i) {
-                                            case BUTTON.A:
-                                                console.log('BUTTON A');
-                                                if (Reveal.isOverview()) {
-                                                    Reveal.toggleOverview();
-                                                } else {
-                                                    Reveal.next();
-                                                }
-                                                break;
-                                            case BUTTON.B:
-                                              console.log('BUTTON X');
-                                                Reveal.prev();
-                                                break;
-                                            case BUTTON.LB:
-                                              console.log('BUTTON SL');
-                                                Reveal.prev();
-                                                break;
-                                            case BUTTON.RB:
-                                              console.log('BUTTON SR');
-                                                Reveal.next();
-                                                break;
-                                            case BUTTON.LT:
-                                              console.log('BUTTON LT');
-                                                Reveal.left();
-                                                break;
-                                            case BUTTON.RT:
-                                              console.log('BUTTON RT');
-                                                Reveal.right();
-                                                break;
-                                            case BUTTON.BACK:
-                                              console.log('BUTTON BACK');
-                                                Reveal.togglePause();
-                                                break;
-                                            case BUTTON.START:
-                                              console.log('BUTTON +');
-                                                Reveal.toggleOverview();
-                                                break;
-                                            case BUTTON.DUP:
-                                              console.log('BUTTON DUP');
-                                                Reveal.up();
-                                                break;
-                                            case BUTTON.DDOWN:
-                                              console.log('BUTTON DDOWN');
-                                                Reveal.down();
-                                                break;
-                                            case BUTTON.DLEFT:
-                                              console.log('BUTTON DLEFT');
-                                                Reveal.left();
-                                                break;
-                                            case BUTTON.DRIGHT:
-                                              console.log('BUTTON DRIGHT');
-                                                Reveal.right();
-                                                break;
-                                            case BUTTON.RIGHTSTICK:
-                                              console.log('BUTTON RIGHTSTICK');
-                                                pointing = !pointing;
-                                                pointer.style.display = pointing ? 'block' : 'none';
-                                                break;
-                                            default:
-                                                console.log('Not mapped', i);
-                                        }
+                                        // handleXbox360Button(i);
+                                        handleRightJoyConButton(i);
                                     }
                                 }
 
@@ -255,36 +217,174 @@
                                         var constrainValue = function constrainValue(newVal) {
                                             return Math.min(Math.max(newVal, 0), window.innerWidth);
                                         };
-                                        switch (i) {
-                                            case AXIS.ROX: {
-                                                var newVal = _val * 2 + left;
-                                                pointer.style.left = constrainValue(newVal) + 'px';
-                                                break;
-                                            }
-                                            case AXIS.ROY: {
-                                                var _newVal = _val * 2 + top;
-                                                pointer.style.top = constrainValue(_newVal) + 'px';
-                                                break;
-                                            }
-                                            case AXIS.LOX: {
-                                                var _newVal2 = _val * fastMultiplier + left;
-                                                pointer.style.left = constrainValue(_newVal2) + 'px';
-                                                break;
-                                            }
-                                            case AXIS.LOY: {
-                                                var _newVal3 = _val * fastMultiplier + top;
-                                                pointer.style.top = constrainValue(_newVal3) + 'px';
-                                                break;
-                                            }
-
-                                            // no default
-                                        }
+                                        // handleXbox360Axis(i, fastMultiplier, left, top, constrainValue);
+                                        handleRightJoyConAxis(i, _val, fastMultiplier, left, top, constrainValue);
                                     }
                                 }
                             }
                         }
 
                         requestAnimationFrame(updateStatus);
+                    }
+
+                    function handleXbox360Axis(i, _val, fastMultiplier, left, top, constrainValue) {
+                        switch (i) {
+                            case AXIS.ROX: {
+                                var newVal = _val * 2 + left;
+                                pointer.style.left = constrainValue(newVal) + 'px';
+                                break;
+                            }
+                            case AXIS.ROY: {
+                                var _newVal = _val * 2 + top;
+                                pointer.style.top = constrainValue(_newVal) + 'px';
+                                break;
+                            }
+                            case AXIS.LOX: {
+                                var _newVal2 = _val * fastMultiplier + left;
+                                pointer.style.left = constrainValue(_newVal2) + 'px';
+                                break;
+                            }
+                            case AXIS.LOY: {
+                                var _newVal3 = _val * fastMultiplier + top;
+                                pointer.style.top = constrainValue(_newVal3) + 'px';
+                                break;
+                            }
+                            // no default
+                        }
+                    }
+
+                    function handleRightJoyConAxis(i, _val, fastMultiplier, left, top, constrainValue) {
+                        switch (i) {
+                            case AXIS.LOX: {
+                                console.log(`i=${i}, axis=LOY`);
+                                var _newVal3 = -_val * fastMultiplier + top;
+                                pointer.style.top = constrainValue(_newVal3) + 'px';
+                                break;
+                            }
+                            case AXIS.LOY: {
+                                console.log(`i=${i}, axis=LOX`);
+                                var _newVal2 = _val * fastMultiplier + left;
+                                pointer.style.left = constrainValue(_newVal2) + 'px';
+                                break;
+                            }
+                            // no default
+                        }
+                    }
+
+                    function handleXbox360Button(button) {
+                        switch (button) {
+                            case BUTTON.A:
+                                console.log('BUTTON A');
+                                if (Reveal.isOverview()) {
+                                    Reveal.toggleOverview();
+                                } else {
+                                    Reveal.next();
+                                }
+                                break;
+                            case BUTTON.B:
+                                console.log('BUTTON X');
+                                Reveal.prev();
+                                break;
+                            case BUTTON.LB:
+                                console.log('BUTTON SL');
+                                Reveal.prev();
+                                break;
+                            case BUTTON.RB:
+                                console.log('BUTTON SR');
+                                Reveal.next();
+                                break;
+                            case BUTTON.LT:
+                                console.log('BUTTON LT');
+                                Reveal.left();
+                                break;
+                            case BUTTON.RT:
+                                console.log('BUTTON RT');
+                                Reveal.right();
+                                break;
+                            case BUTTON.BACK:
+                                console.log('BUTTON BACK');
+                                Reveal.togglePause();
+                                break;
+                            case BUTTON.START:
+                                console.log('BUTTON +');
+                                Reveal.toggleOverview();
+                                break;
+                            case BUTTON.DUP:
+                                console.log('BUTTON DUP');
+                                Reveal.up();
+                                break;
+                            case BUTTON.DDOWN:
+                                console.log('BUTTON DDOWN');
+                                Reveal.down();
+                                break;
+                            case BUTTON.DLEFT:
+                                console.log('BUTTON DLEFT');
+                                Reveal.left();
+                                break;
+                            case BUTTON.DRIGHT:
+                                console.log('BUTTON DRIGHT');
+                                Reveal.right();
+                                break;
+                            case BUTTON.RIGHTSTICK:
+                                console.log('BUTTON RIGHTSTICK');
+                                pointing = !pointing;
+                                pointer.style.display = pointing ? 'block' : 'none';
+                                break;
+                            default:
+                                console.log('Not mapped', button);
+                        }
+                    }
+
+                    function handleRightJoyConButton(button) {
+                        switch (button) {
+                            case RJ_BUTTON.A:
+                                console.log('BUTTON A');
+                                Reveal.right();
+                                break;
+                            case RJ_BUTTON.B:
+                                console.log('BUTTON B');
+                                Reveal.down();
+                                break;
+                            case RJ_BUTTON.X:
+                                console.log('BUTTON X');
+                                Reveal.prev();
+                                break;
+                            case RJ_BUTTON.Y:
+                                console.log('BUTTON Y');
+                                Reveal.left();
+                                break;
+                            case RJ_BUTTON.SL:
+                                console.log('BUTTON SL');
+                                Reveal.prev();
+                                break;
+                            case RJ_BUTTON.SR:
+                                console.log('BUTTON SR');
+                                Reveal.next();
+                                break;
+                            case RJ_BUTTON.R:
+                                console.log('BUTTON R');
+                                Reveal.toggleOverview();
+                                break;
+                            case RJ_BUTTON.ZR:
+                                console.log('BUTTON ZR');
+                                if (Reveal.isOverview()) {
+                                    Reveal.toggleOverview();
+                                } else {
+                                    Reveal.next();
+                                }
+                                break;
+                            case RJ_BUTTON.PLUS:
+                                console.log('BUTTON +');
+                                Reveal.togglePause();
+                                break;
+                            case RJ_BUTTON.STICK:
+                                console.log('BUTTON STICK "CLICK"');
+                                pointing = !pointing;
+                                pointer.style.display = pointing ? 'block' : 'none';
+                                break;
+                            default:
+                                console.log('Not mapped', button);
+                        }
                     }
 
                     function connecthandler(e) {
@@ -305,7 +405,7 @@
                     if (!haveEvents) {
                         setInterval(scangamepads, 500);
                     }
-                };
+                }
                 window.initGamepad = init;
             },
             {}
