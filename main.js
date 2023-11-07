9fafd6d1148c7cf7edf25545b9e0d2af348a2168
Audren <audren.burlot@gmail.com>
2023-11-07 17:52:46 +0100

    prep jug nantes

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
 import 'reveal.js/dist/reveal.css'
-// see available themes in the
-// node_modules/reveal.js/dist/theme
-//  beige, black, blood, league, moon, night, serif, simple, ...
 import 'reveal.js/dist/theme/blood.css'
 import 'reveal.js/plugin/highlight/monokai.css'
 import './style.css'
@@ -10,6 +7,7 @@ import Reveal from 'reveal.js'
 import RevealNotes from 'reveal.js/plugin/notes/notes'
 import RevealHighlight from 'reveal.js/plugin/highlight/highlight'
 import RevealMath from 'reveal.js/plugin/math/math'
+import RevealJoyCon from 'reveal.js-joycon-plugin'
 
 const deck = new Reveal()
 deck.initialize({
@@ -19,10 +17,16 @@ deck.initialize({
         RevealNotes,
         RevealHighlight,
         RevealMath.KaTeX,
+        RevealJoyCon
     ],
-    dependencies: [
-        { src: 'plugins/gamepad/gamepad.js', async: true, callback: () => initGamepad(deck)}
-    ]
+    /* JoyCon plugin options */
+    joycon: {
+        cooldown: 200,
+        pointerSpeed: 15
+    }
+    // dependencies: [
+    //     { src: 'plugins/gamepad/gamepad.js', async: true, callback: () => initGamepad(deck)}
+    // ]
 })
 
 // automatically load the initial fragment on a slide if the fragment
